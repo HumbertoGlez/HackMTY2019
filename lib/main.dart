1655fbdab12ef7c549ff845a8cb891845f838d05
Humberto Gonzalez <hglez.98s@gmail.com>
2019-08-25 01:31:22 -0500

    Added ui for expenses tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,371 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() => runApp(MyApp());
-
+Color konfioPurple = const Color.fromARGB(0xFF, 0x72, 0x64, 0xc9);
+Color konfioDarkPurple = const Color.fromARGB(0xFF, 0x57, 0x4b, 0xa8);
+Color konfioLightPurple = const Color.fromARGB(0xFF, 0xB4, 0xC5, 0xF1);
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static const String _title = 'Asesor Financiero Pyme';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Asesor Financiero Konfío',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        fontFamily: 'Montserrat',
-        primarySwatch: Colors.purple,
-      ),
-      home: MyHomePage(title: 'Asesor Financiero'),
+      title: _title,
+      theme: ThemeData(fontFamily: 'Montserrat'),
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/**
+ *  Facturación
+ */
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Gastos',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Facturación',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Ventas',
+      style: optionStyle,
+    ),
+  ];
 
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+//      appBar: AppBar(
+//        title: const Text('Asesor Financiero'),
+//        backgroundColor: konfioPurple,
+//      ),
+      body: _buildBody(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money_off),
+            title: Text('Gastos'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assessment),
+            title: Text('Facturación'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            title: Text('Ventas'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: konfioPurple,
+        onTap: _onItemTapped,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildBody(int index) {
+    double cardHeight = 200;
+    switch (index) {
+      case 0: // Gastos
+        return
+          Container(
+          child: StaggeredGridView.count(
+          crossAxisCount: 1,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            children: <Widget>[
+              /// NUMERO DE PROVEEDORES
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Material(
+                      color: konfioDarkPurple,
+                      elevation: 14.0,
+                      borderRadius: BorderRadius.circular(24.0),
+                      shadowColor: Color(0x802196F3),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.directions_boat,
+                            color: konfioLightPurple,
+                            size: 50,
+                          ),
+                          Text(
+                            '1179',
+                            overflow: TextOverflow.ellipsis,
+                            textScaleFactor: 3,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            'Proveedores en el periodo',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: Colors.white),
+                            textScaleFactor: 1.5,
+                          )
+                        ],
+                      )
+                  )
+              ),
+              ///  GASTO MAS GRANDE
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Material(
+                    color: Colors.white,
+                    elevation: 14.0,
+                    borderRadius: BorderRadius.circular(24.0),
+                    shadowColor: Color(0x802196F3),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          Icons.event_available,
+                          color: konfioLightPurple,
+                          size: 50,
+                        ),
+                        Text(
+                          "\$11,005,280",
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 2,
+                          style: TextStyle(color: konfioPurple),
+                        ),
+                        Text(
+                          'Gasto más grande del periodo',
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          'google operaciones de méxico, s. de r.l. de c.v.',
+                          overflow: TextOverflow.visible,
+                          textScaleFactor: 1.5,
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  )
+              ),
+              /// PROVEEDOR MAS GRANDE
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Material(
+                    color: Colors.white,
+                    elevation: 14.0,
+                    borderRadius: BorderRadius.circular(24.0),
+                    shadowColor: Color(0x802196F3),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: konfioLightPurple,
+                          size: 50,
+                        ),
+                        Text(
+                          "\$263,719,700",
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 2,
+                          style: TextStyle(color: konfioPurple),
+                        ),
+                        Text(
+                          'Proveedor más grande del periodo',
+                          overflow: TextOverflow.ellipsis,
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          'red amigo dal sapi de cv sofom enr',
+                          overflow: TextOverflow.visible,
+                          textScaleFactor: 1.5,
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  )
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Material(
+                  color: Colors.white,
+                  elevation: 14.0,
+                  shadowColor: Color(0x802196F3),
+                  child: ListView(
+                    children: [
+                      Text(
+                        'Facturas de gastos más altos del periodo',
+                        style: TextStyle(color: konfioPurple),
+                        textScaleFactor: 2,
+                      ),
+                      Table(
+                      border: TableBorder.all(),
+                      children: [
+                        TableRow(
+                            children: [
+                              Text(
+                                'Fecha',
+                                style: TextStyle(fontStyle: FontStyle.italic, color: konfioPurple),
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                'RFC',
+                                style: TextStyle(fontStyle: FontStyle.italic, color: konfioPurple),
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                'Razón Social',
+                                style: TextStyle(fontStyle: FontStyle.italic, color: konfioPurple),
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                'Importe',
+                                style: TextStyle(fontStyle: FontStyle.italic, color: konfioPurple),
+                                textAlign: TextAlign.center,
+                              )
+                            ]
+                        ),
+                        TableRow(
+                            children: [
+                              Text(
+                                '1/11/18',
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                'GOM0809114P5',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                'Google Operaciones de México',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                '\$11,005.28',
+                                textAlign: TextAlign.center,
+                              )
+                            ]
+                        ),
+                        TableRow(
+                            children: [
+                              Text(
+                                '1/02/19',
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                'GOM0809114P5',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                'Google Operaciones de México',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                '\$10,282.169',
+                                textAlign: TextAlign.center,
+                              )
+                            ]
+                        ),
+                        TableRow(
+                            children: [
+                              Text(
+                                '2/12/18',
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                'GOM0809114P5',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                'Google Operaciones de México',
+                                textAlign: TextAlign.center,
+
+                              ),
+                              Text(
+                                '\$8,597.975',
+                                textAlign: TextAlign.center,
+                              )
+                            ]
+                        )
+                      ],
+                    )
+                  ]
+                )
+                  )
+                )
+              ],
+              staggeredTiles: [
+                StaggeredTile.extent(1, 200),
+                StaggeredTile.extent(1, 250),
+                StaggeredTile.extent(1, 250),
+                StaggeredTile.extent(1, 300)
+              ],
+            )
+        );
+        break;
+
+      case 1: // Facturacion
+        return Stack(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: [
+                  Card(
+                    child: Column(
+
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+        break;
+
+      case 2: // Ventas
+        return Stack(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: [
+                  Card(
+                    child: Column(
+
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+        break;
+        
+      default: 
+        return Container(
+            child: Text('Error!')
+        );
+    }
   }
 }
